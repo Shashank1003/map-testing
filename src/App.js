a16e7c905609471ef66d7f1cdb1f5b035512ced5
Shashank Gupta <shashank.gupta@dronebase.com>
2022-11-28 09:11:07 +0530

    PBF

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "mapbox-gl/dist/mapbox-gl.css";
 
-function App() {
+import Map, { Layer, Source } from "react-map-gl";
+import { useRef, useState } from "react";
+
+const MAPBOX_TOKEN =
+  "pk.eyJ1IjoiYWlycHJvYmUiLCJhIjoiY2tkcmVqbDF2MDVqbzJ0b3FmeTcxcHFrZSJ9.YQR_ZeBEF43y8pV2KKvHcg";
+
+export default function App() {
+  const [viewPort, setViewPort] = useState({
+    longitude: -0.795884491,
+    latitude: 50.76080173,
+    zoom: 15,
+    minZoom: 12,
+  });
+  const mapRef = useRef(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Map
+      {...viewPort}
+      onMove={(e) => {
+        setViewPort(e.viewState);
+      }}
+      style={{ height: `97vh`, width: `98vw` }}
+      // initialViewState={viewPort}
+      mapStyle="mapbox://styles/mapbox/satellite-v9"
+      mapboxAccessToken={MAPBOX_TOKEN}
+      ref={mapRef}
+      // onLoad={mapLoad}
+      // onMouseMove={handleMouseMove}
+      onClick={(e) => {
+        console.log(mapRef.current.getMap().queryRenderedFeatures(e.point));
+      }}
+      // onMouseOut={closeAllActions}
+      // onStyleData={handleStyleChange}
+      // onTouchStart={onClickMarkerEvent}
+    >
+      <Source
+        id="mvt"
+        type="vector"
+        tiles={[
+          "http://20.219.131.227:8000/tiles/{z}/{x}/{y}",
+          "http://20.219.131.227:8000/tiles/{z}/{x}/{y}",
+          "http://20.219.131.227:8000/tiles/{z}/{x}/{y}",
+          "http://20.219.131.227:8000/tiles/{z}/{x}/{y}",
+        ]}
+      />
+
+      <Layer
+        id="mvt"
+        source="mvt"
+        source-layer="default"
+        type="fill"
+        paint={{
+          "fill-color": "#00ffff",
+          "fill-opacity": 0.5,
+        }}
+      />
+    </Map>
   );
 }
-
-export default App;
