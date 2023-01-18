45eb885edc370daeef8458dba20f2a724f80f356
Shashank Gupta <guptashashank8975@gmail.com>
2023-01-18 11:11:38 +0530

    added table-id api in panel multiselect
    Signed-off-by: Shashank Gupta <guptashashank8975@gmail.com>

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "mapbox-gl/dist/mapbox-gl.css";
 import Map, { Layer, Source } from "react-map-gl";
 import { useEffect, useRef, useState } from "react";
 
+import axios from "axios";
+
 const MAPBOX_TOKEN =
   "pk.eyJ1IjoiYWlycHJvYmUiLCJhIjoiY2tkcmVqbDF2MDVqbzJ0b3FmeTcxcHFrZSJ9.YQR_ZeBEF43y8pV2KKvHcg";
 
@@ -23,6 +25,7 @@ export default function App() {
   const [endPos, setEndPos] = useState([]);
   const [tableConfig, setTableConfig] = useState([]);
   const [panelsInTable, setPanelsInTable] = useState([]);
+  const [selectedTableId, setSelectedTableId] = useState(null);
 
   // useEffect(() => {
   //   console.log("FEATURE UPDATED", featureData);
@@ -33,47 +36,73 @@ export default function App() {
   }, [isSelectingMultiple]);
 
   useEffect(() => {
-    if (startPos.length && endPos.length) {
+    const fetchPanels = async () => {
+      if (selectedTableId) {
+        const apiData = await axios
+          .get(
+            `http://ec2-18-194-188-171.eu-central-1.compute.amazonaws.com:8000/tables/${selectedTableId}`
+          )
+          .then((resp) => {
+            // console.log("resp", resp);
+            setPanelsInTable(resp.data.features);
+            // panelsInTable = resp.data.features;
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+        // console.log("API DATA", apiData);
+      }
+    };
+    fetchPanels();
+  }, [selectedTableId]);
+
+  useEffect(() => {
+    // console.log("PANELS IN TABLE OUTSIDE", panelsInTable);
+
+    if (startPos.length && endPos.length && panelsInTable.length) {
+      // console.log("PANELS IN TABLE", panelsInTable);
       if (startPos[0] > endPos[0]) {
         const startPosition = startPos;
         setStartPos(endPos);
         return setEndPos(startPosition);
       }
       const tableRow = parseInt(tableConfig[1]);
-      console.log(panelsInTable);
+      // console.log(panelsInTable);
       console.log(startPos, endPos, tableConfig, tableRow);
       const filteredData = panelsInTable.filter((x) => {
         if (startPos[0] !== endPos[0]) {
-          if (x.properties.panel_column === startPos[0]) {
-            return x.properties.panel_row >= startPos[1];
-          } else if (x.properties.panel_column === endPos[0]) {
-            return x.properties.panel_row <= endPos[1];
+          if (x.properties.column_position === startPos[0]) {
+            return x.properties.row_position >= startPos[1];
+          } else if (x.properties.column_position === endPos[0]) {
+            return x.properties.row_position <= endPos[1];
           } else {
             return (
-              x.properties.panel_column > startPos[0] &&
-              x.properties.panel_column < endPos[0]
+              x.properties.column_position > startPos[0] &&
+              x.properties.column_position < endPos[0]
             );
           }
         } else {
           return (
-            x.properties.panel_column === startPos[0] &&
-            x.properties.panel_row >= startPos[1] &&
-            x.properties.panel_row <= endPos[1]
+            x.properties.column_position === startPos[0] &&
+            x.properties.row_position >= startPos[1] &&
+            x.properties.row_position <= endPos[1]
           );
         }
       });
-      console.log("Filtered data", filteredData);
+      console.log("Filtered data", filteredData, panelsInTable);
       setFeatureData({
         type: "FeatureCollection",
         features: filteredData,
       });
       setStartPos([]);
       setEndPos([]);
+      setSelectedTableId(null);
       setPanelsInTable([]);
     }
-  }, [startPos, endPos]);
+  }, [startPos, endPos, panelsInTable]);
 
-  const handleMapClick = (event) => {
+  const handleMapClick = async (event) => {
+    // mapRef.current.getMap().moveLayer("mvt");
     // console.log(mapRef.current.getMap().queryRenderedFeatures(event.point));
     const feature = mapRef.current
       .getMap()
@@ -92,25 +121,25 @@ export default function App() {
           features: [featureObj],
         });
       } else {
-        const listOfPanels = mapRef.current
-          .getMap()
-          .querySourceFeatures("mvt", {
-            sourceLayer: "default",
-            filter: ["==", "table_id", feature.properties.table_id],
-          });
+        // const listOfPanels = mapRef.current
+        //   .getMap()
+        //   .querySourceFeatures("mvt", {
+        //     sourceLayer: "default",
+        //     filter: ["==", "table_id", feature.properties.table_id],
+        //   });
         if (!startPos.length) {
           setStartPos([
             feature.properties.panel_column,
             feature.properties.panel_row,
           ]);
         } else {
+          setSelectedTableId(feature.properties.table_id);
           setEndPos([
             feature.properties.panel_column,
             feature.properties.panel_row,
           ]);
         }
         setTableConfig(feature.properties.table_config.split("x"));
-        setPanelsInTable(listOfPanels);
       }
     }
   };
@@ -139,6 +168,7 @@ export default function App() {
       <Map
         {...viewPort}
         onMove={(e) => {
+          console.log(e.viewState.zoom);
           setViewPort(e.viewState);
         }}
         style={{ height: `100vh`, width: `100%` }}
@@ -152,10 +182,11 @@ export default function App() {
           type="vector"
           tiles={[
             "http://ec2-18-194-188-171.eu-central-1.compute.amazonaws.com:8000/tiles/{z}/{x}/{y}",
-            "http://ec2-18-194-188-171.eu-central-1.compute.amazonaws.com:8000/tiles/{z}/{x}/{y}",
-            "http://ec2-18-194-188-171.eu-central-1.compute.amazonaws.com:8000/tiles/{z}/{x}/{y}",
-            "http://ec2-18-194-188-171.eu-central-1.compute.amazonaws.com:8000/tiles/{z}/{x}/{y}",
           ]}
+          // scheme="tms"
+          maxzoom={24}
+          tileSize={512}
+          // volatile={true}
         />
 
         <Layer
@@ -223,6 +254,18 @@ export default function App() {
           type="symbol"
           layout={{
             "text-field": ["get", "panel_row"],
+            "text-offset": [0, viewPort.zoom / (viewPort.zoom + 10)],
+            "text-size": viewPort.zoom > 20 ? viewPort.zoom / 2 : 0,
+            "text-allow-overlap": true,
+          }}
+        />
+
+        <Layer
+          id="highlightTextMultiple"
+          source="highlight"
+          type="symbol"
+          layout={{
+            "text-field": ["get", "row_position"],
             "text-offset": [0, viewPort.zoom / (viewPort.zoom + 10)],
             "text-size": viewPort.zoom > 20 ? viewPort.zoom / 2 : 0,
             "text-allow-overlap": true,
